022eca2a6623fa57c50ff8ff3bb3009eab59623c
Zibelas <31689279+Zibelas@users.noreply.github.com>
2020-11-23 09:40:02 +0100

    Init commit

---
+++ Assets/PlayerInputActions.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""794bb1cd-cc77-49fd-bed1-95cad9d2a896"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""001ce2e7-35d5-42ed-9e33-486271c3e8bd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""5c4e5336-76dc-4c9b-9d33-6a8d3636e249"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Jump;
+    public struct PlayerActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/QueuedJump.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QueuedJump: MonoBehaviour, PlayerInputActions.IPlayerActions
+{
+   
+    [SerializeField] LayerMask groundLayers;
+    [SerializeField] float jumpPressedRememberTime = 0.2f;
+    [SerializeField] float groundedRememberTime = 0.25f;  
+    [SerializeField] [Range(0, 10)] float jumpHeight = 5f;
+
+    private Rigidbody2D _rb;
+    private PlayerInputActions _controls; 
+    private Vector2 _playerSize;
+    private float _jumpPressedRemember = 0;
+    private float _groundedRemember = 0;
+   
+    private void Awake() 
+    {
+        _controls = new PlayerInputActions();    
+        _controls.Player.SetCallbacks(this);
+        _controls.Enable();
+    }
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();    
+        _playerSize = (Vector2) GetComponent<SpriteRenderer>().bounds.size + new Vector2(-0.02f, 0);
+    }
+   
+    void PlayerInputActions.IPlayerActions.OnJump(InputAction.CallbackContext context){
+        switch (context.phase) {
+            case InputActionPhase.Performed:
+                _jumpPressedRemember = jumpPressedRememberTime;
+                break;
+        }
+    }
+
+    void Update(){ 
+        Vector2 groundedBoxCheckPosition = (Vector2) transform.position + new Vector2(0, -0.01f);
+        bool grounded = Physics2D.OverlapBox(groundedBoxCheckPosition, _playerSize, 0, groundLayers);
+        _groundedRemember -= Time.deltaTime;
+        if (grounded)
+        {
+            _groundedRemember = groundedRememberTime;
+        }
+        _jumpPressedRemember -= Time.deltaTime;
+        if ((_jumpPressedRemember > 0) && (_groundedRemember > 0))
+        {
+            _rb.velocity = Vector3.zero;
+            _rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            _groundedRemember = 0;
+            _jumpPressedRemember = 0;
+        }
+    }   
+}
